f1cf7e53ede6a9d33a85d1dfce86aab8017a3f2f
Bernard <W0453579@campus.nscc.ca>
2025-03-05 10:32:02 -0400

    Initial commit

---
+++ Game1.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Nez;
+using Nez.Sprites;
+using Nez.Textures;
+
+
+
+namespace MonoGame_Framework
+{
+    public class Game1 : Core
+    {
+        //private GraphicsDeviceManager _graphics;
+        //private SpriteBatch _spriteBatch;
+
+        public Game1() : base()
+        {
+            /*
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+            */
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+            var scene = Scene.CreateWithDefaultRenderer(Color.BurlyWood);
+
+            var Player = scene.Content.Load<Texture2D>("Player");
+            //var scene = Scene.Create(Color.BurlyWood);
+            var entity = scene.CreateEntity("First-Sprite");
+            var sprite = new Sprite(Player, new Rectangle(50, 50, Player.Width, Player.Height));
+            entity.AddComponent<Sprite>(new Sprite(Player));
+            Scene = scene;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new MonoGame_Framework.Game1();
+game.Run();
