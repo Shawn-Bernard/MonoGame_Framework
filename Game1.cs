23cfa6f98775c5be7c4d59444082a27ed11fafb5
Shawn Bernard <ssbernard@outlook.com>
2025-03-17 22:08:39 -0300

    Added Player entity with Movement

--- Game1.cs
+++ Game1.cs
@@ -1,146 +1,119 @@
 ﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 using Nez;
 using Nez.Sprites;
 using Nez.Textures;
 using System;
 using System.Collections.Generic;
-
+using System.Security;
+using Nez.AI.UtilityAI;
 
 
 namespace MonoGame_Framework
 {
     public class Game1 : Core
     {
-        private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
+        //private GraphicsDeviceManager _graphics;
+        //private SpriteBatch _spriteBatch;
 
-        public Game1()
+        public Game1() : base()
         {
-            _graphics = new GraphicsDeviceManager(this);
+            //_graphics = new GraphicsDeviceManager(this);
             //Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-            //SpriteBatch
-            /*
             base.Initialize();
-            var scene = Scene.CreateWithDefaultRenderer(Color.BurlyWood);
-            
-            var Player = scene.Content.Load<Texture2D>("Player");
-            //var scene = Scene.Create(Color.BurlyWood);
-            //var entity = scene.CreateEntity("First-Sprite");
-            //Entity player = scene.AddEntity(entity);
-            var player = new Entity();
-            
-            scene.AddEntity(player);
 
-            //Component _player = player.AddComponent<Component>();
-            
-            var sprite = new Sprite(Player, new Rectangle(50, 50, Player.Width, Player.Height));
-            //Console.Write($"{entity.Components}");
-            //Debug.DrawText($"{entity.Components}");
-            var boom = player.AddComponent<Component>();
-            //boom.Entity.Transform.
-            //entity.Components
-            //player
-            scene.Begin();
+            // Making a new scene with default renderer for background
+            Scene scene = Scene.CreateWithDefaultRenderer(Color.Red);
+            scene.ClearColor = Color.Red;
+
+            // Create player entity
+            Entity player = new Entity();
+            player = scene.AddEntity(player);
+            //player = scene.CreateEntity("Player");
+
+            // Loads player texture 
+            Texture2D playerTexture = scene.Content.Load<Texture2D>("Player");
+
+            // Adding a new sprite renderer component and adding my player texture
+            player.AddComponent(new SpriteRenderer(playerTexture));
+
+
+            // Adds player movement component to player
+            player.AddComponent(new PlayerMovement());
+
+            // This gets the player movement component in _player, no need for this just wanna try it
+            PlayerMovement _player = player.GetComponent<PlayerMovement>();
+
+            // Setting core Scene to my new scene that I made
             Scene = scene;
-
-            scene.Begin();
-            scene.Initialize();
-            scene.Update();
-
-            scene.End();
-            */
         }
-        public class Actor
+
+        // Player Movement Component
+        public class PlayerMovement : Component, IUpdatable
         {
-            public Entity entity;
-            public bool isTurn;
-            public bool WaitForTurn;
-            public bool WaitAnimation;
-            double animation;
+            private int speed = 100;
+            Entity player;
 
-            public virtual void StartTurn()
+            SpriteRenderer spriteRenderer;
+            // This is basically start method
+            public override void OnAddedToEntity()
             {
-                //change color 
-                //entity.GetComponent<>
-                isTurn = true;
-                WaitForTurn = false;
+                Transform.Position = new Vector2 (50, 50);
+                Transform.Scale = new Vector2(5, 5);
+
+                spriteRenderer = this.GetComponent<SpriteRenderer>();
+                player = Transform.Entity;
+
+                //Changing scene color 
+                player.Scene.ClearColor = Color.Plum;
+                Debug.Log("Test for debug");
+
             }
-
-            public virtual void EndTurn()
+            //Component, IUpdatable lets me use the update method
+            public void Update()
             {
-                isTurn = false;
-                WaitForTurn = true;
-            }
-
-            public virtual void UpdateTurn(GameTime gameTime)
-            {
-                if (WaitForTurn)
+                Vector2 move = Vector2.Zero;
+                if (Input.IsKeyPressed(Keys.W))
                 {
-                    animation += gameTime.TotalGameTime.TotalSeconds;
-                    if (animation < 15f)
-                    {
-
-                    }
-                    else
-                    {
-                        isTurn = true;
-                    }
+                    move.Y -= 100;
+                    spriteRenderer.Color = Color.Plum;
                 }
-                
-                
-                
-
-            }
-
-            public virtual void Move(GameTime gameTime, Vector2 changeVector)
-            {
-                animation = gameTime.TotalGameTime.TotalSeconds;
-                Vector2 MoveVector = new Vector2(entity.Position.X + changeVector.X, entity.Position.Y + changeVector.Y);
-                entity.TweenPositionTo(MoveVector, (float)animation).Start();//SetCompletionHandler();
-                
-                WaitAnimation = true;
-
-            }
-
-        }
-        public class turnManager
-        {
-            List<Actor> entities = new List<Actor>();
-
-            public int order = 0;
-
-            public void updateturn()
-            {
-                if (order < entities.Count && order >= entities.Count)
+                if (Input.IsKeyPressed(Keys.S))
                 {
-                    var ActorTurn = entities[order];
-                    if (ActorTurn.isTurn)
-                    {
-                        ActorTurn.StartTurn();
-                    }
-                    else
-                    {
-                        ActorTurn.EndTurn();
-                    }
-                    order++;
+                    move.Y += 100;
+                    spriteRenderer.Color = Color.Gold;
+                }
+                if (Input.IsKeyPressed(Keys.A))
+                {
+                    move.X -= 100;
+                    spriteRenderer.Color = Color.RoyalBlue;
+                }
+                if (Input.IsKeyPressed(Keys.D))
+                {
+                    move.X += 100;
+                    spriteRenderer.Color = Color.Tomato;
+                }
+                if (Input.IsKeyDown(Keys.LeftShift))
+                {
+                    speed = 200;
                 }
                 else
                 {
-                    order = 0;
-                    var actor = entities[order];
+                    speed = 100;
                 }
+                // This gives a more player snap
+                player.TweenLocalPositionTo(Transform.Position += move, 0.2f);
+
+                // This moves the player like normal
+                // Transform.Position += move * speed * Time.DeltaTime;
             }
-
-
-
         }
     }
 }
